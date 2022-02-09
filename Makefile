eaddb98c0c2d06fe386f403328751d0be1db0015
Tom Wilson <tom@hyper.io>
2022-02-09 18:21:30 +0000

    chore: stubbed out functions #11

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 test:
 	@deno test --import-map import_map.json
+
+bundle:
+	@deno bundle --import-map import_map.json src/mod.ts dist/bundle.js
--- src/mod_test.ts
+++ src/mod_test.ts
@@ -1,5 +1,46 @@
-import { assertEquals } from 'asserts'
+import { assertEquals } from "asserts";
+import { model } from './mod.ts'
 
-Deno.test('ok', () => {
-  assertEquals(true, true)
+Deno.test("ok", () => {
+  assertEquals(true, true);
+});
+
+const hyper = model({})
+const profiles = hyper.ext.model()
+
+
+Deno.test('upsert document successfully', async () => {
+  const result = await profiles.upsert('1', { username: 'rakis'})
+  assertEquals(result.ok, false)
+  assertEquals(result.msg, 'Not Implemented!')
+})
+
+Deno.test('get document successfully', async () => {
+  const result = await profiles.get('1')
+  assertEquals(result.ok, false)
+  assertEquals(result.msg, 'Not Implemented!')
+})
+
+Deno.test('remove document successfully', async () => {
+  const result = await profiles.remove('1')
+  assertEquals(result.ok, false)
+  assertEquals(result.msg, 'Not Implemented!')
+})
+
+Deno.test('query document successfully', async () => {
+  const result = await profiles.query({group: 'admin'}, { limit: 10})
+  assertEquals(result.ok, false)
+  assertEquals(result.msg, 'Not Implemented!')
+})
+
+Deno.test('search document successfully', async () => {
+  const result = await profiles.search('rakis')
+  assertEquals(result.ok, false)
+  assertEquals(result.msg, 'Not Implemented!')
+})
+
+Deno.test('sync document successfully', async () => {
+  const result = await profiles.sync()
+  assertEquals(result.ok, false)
+  assertEquals(result.msg, 'Not Implemented!')
 })
