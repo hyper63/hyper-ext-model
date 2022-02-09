e1346a0e5e042cf4d95361717f697d153b3c3d48
Tom Wilson <tom@hyper.io>
2022-02-09 19:47:24 +0000

    feature(asyncify): evolve hyper object to Asyncs #2

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 test:
-	@deno test --import-map import_map.json
+	@deno fmt src --check && deno lint src && deno test --import-map import_map.json src
 
 bundle:
 	@deno bundle --import-map import_map.json src/mod.ts dist/bundle.js
--- src/asyncify.ts
+++ src/asyncify.ts
@@ -1,12 +1,42 @@
-import { evolve } from 'ramda'
-import { Async } from 'crocks'
+import { evolve } from "ramda";
+import { Async } from "crocks";
 
-
-export const asyncify = (hyper) {
+export const asyncify = (hyper: unknown) => {
   const transformations = {
     data: {
       add: Async.fromPromise,
-      get: Async.fromPromise
-    }
-  }
-}
+      get: Async.fromPromise,
+      update: Async.fromPromise,
+      remove: Async.fromPromise,
+      query: Async.fromPromise,
+      bulk: Async.fromPromise,
+      index: Async.fromPromise,
+    },
+    cache: {
+      add: Async.fromPromise,
+      get: Async.fromPromise,
+      update: Async.fromPromise,
+      remove: Async.fromPromise,
+      query: Async.fromPromise,
+    },
+    search: {
+      add: Async.fromPromise,
+      remove: Async.fromPromise,
+      get: Async.fromPromise,
+      update: Async.fromPromise,
+      query: Async.fromPromise,
+      load: Async.fromPromise,
+    },
+    storage: {
+      upload: Async.fromPromise,
+      download: Async.fromPromise,
+      remove: Async.fromPromise,
+    },
+    queue: {
+      enqueue: Async.fromPromise,
+      errors: Async.fromPromise,
+      queued: Async.fromPromise,
+    },
+  };
+  return evolve(transformations, hyper);
+};
